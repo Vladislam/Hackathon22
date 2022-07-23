5418f7e5e8ba1a604b4a84aeaa4d5099f9690cd6
Stanley <stenleone@gmail.com>
2022-07-23 16:44:53 +0300

    add save manager add file picker

---
+++ app/src/main/java/com/dungeon/software/hackathon/data/repository/StorageDataSource.kt
@@ -0,0 +1,41 @@
+package com.dungeon.software.hackathon.data.repository
+
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+class StorageDataSource {
+
+    private val storage = FirebaseStorage.getInstance()
+    private val storageRef = storage.reference
+
+    suspend fun saveFile(file: File) = suspendCoroutine { emitter ->
+        val user = "Firebase.auth.currentUser" ?: run {
+            emitter.resumeWithException(IllegalArgumentException())
+            return@suspendCoroutine
+        }
+
+        val stream = FileInputStream(file)
+        val ref = storageRef.child("user${"user.uid"}/file/${file.path}/${UUID.randomUUID()}")
+        ref.putStream(stream)
+            .addOnFailureListener {
+                emitter.resumeWithException(it)
+            }
+            .addOnSuccessListener { taskSnapshot ->
+                ref.downloadUrl
+                    .addOnSuccessListener {
+                        emitter.resume(it.toString())
+                    }
+                    .addOnFailureListener {
+                        emitter.resumeWithException(it)
+                    }
+            }
+    }
+
+}
--- app/src/main/java/com/dungeon/software/hackathon/di/modelModeule.kt
+++ app/src/main/java/com/dungeon/software/hackathon/di/modelModeule.kt
@@ -1,7 +1,8 @@
 package com.dungeon.software.hackathon.di
 
+import com.dungeon.software.hackathon.data.repository.StorageDataSource
 import org.koin.dsl.module
 
 val modelModule = module {
-
+    single { StorageDataSource() }
 }
