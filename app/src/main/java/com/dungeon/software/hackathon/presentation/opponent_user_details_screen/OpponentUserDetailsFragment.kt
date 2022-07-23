50a1a95bad66e72ef97eecd3f5af88a523537cfb
Serhii Leskov <serhii.leskov-ext@udelphi.com>
2022-07-23 17:17:11 +0300

    Opponent User Details UI

--- app/src/main/java/com/dungeon/software/hackathon/presentation/opponent_user_details_screen/OpponentUserDetailsFragment.kt
+++ app/src/main/java/com/dungeon/software/hackathon/presentation/opponent_user_details_screen/OpponentUserDetailsFragment.kt
@@ -6,7 +6,7 @@ import com.dungeon.software.hackathon.databinding.FragmentChatBinding
 import com.dungeon.software.hackathon.databinding.FragmentOpponentUserDetailsBinding
 import kotlin.reflect.KClass
 
-class OpponentUserDetailsScreen : BaseVMFragment<OpponentUserDetailsViewModel, FragmentOpponentUserDetailsBinding>() {
+class OpponentUserDetailsFragment : BaseVMFragment<OpponentUserDetailsViewModel, FragmentOpponentUserDetailsBinding>() {
     override val layoutId: Int
         get() = R.layout.fragment_opponent_user_details
     override val viewModelClass: KClass<OpponentUserDetailsViewModel>
