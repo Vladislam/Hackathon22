af1eb0cc6fcbf31f1b41dc5b873b3965b464d794
Stanley <stenleone@gmail.com>
2022-07-23 17:45:33 +0300

    add bottom navigation to nav component

--- app/src/main/java/com/dungeon/software/hackathon/presentation/MainActivity.kt
+++ app/src/main/java/com/dungeon/software/hackathon/presentation/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.dungeon.software.hackathon.presentation
 
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.dungeon.software.hackathon.R
 import com.dungeon.software.hackathon.base.activity.BaseBindingActivity
 import com.dungeon.software.hackathon.databinding.ActivityMainBinding
@@ -12,6 +14,8 @@ class MainActivity : BaseBindingActivity<ActivityMainBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.bottomNavigation.setupWithNavController((supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment).navController)
     }
 
     fun changeBottomNavigationVisibility(visibility: Int) {
