eed66f808a1f16212f9cb373308ce23509c19d65
Stanley <stenleone@gmail.com>
2022-07-24 13:32:59 +0300

    add log out google

--- app/src/main/java/com/dungeon/software/hackathon/presentation/user_details_screen/UserDetailsViewModel.kt
+++ app/src/main/java/com/dungeon/software/hackathon/presentation/user_details_screen/UserDetailsViewModel.kt
@@ -1,17 +1,27 @@
 package com.dungeon.software.hackathon.presentation.user_details_screen
 
+import android.content.Context
 import android.net.Uri
+import com.dungeon.software.hackathon.R
 import com.dungeon.software.hackathon.base.view_model.BaseViewModel
 import com.dungeon.software.hackathon.domain.models.User
 import com.dungeon.software.hackathon.domain.repository.AuthRepository
 import com.dungeon.software.hackathon.domain.repository.UserRepository
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.Api.ApiOptions.HasOptions
+import com.google.android.gms.common.api.GoogleApiClient
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.tasks.await
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
+
 
 class UserDetailsViewModel(
     private val userRepository: UserRepository,
     private val authRepository: AuthRepository
-) : BaseViewModel() {
+) : BaseViewModel(), KoinComponent {
 
     private val _currentUser = MutableSharedFlow<User>()
     val currentUser: SharedFlow<User> = _currentUser
@@ -37,6 +47,10 @@ class UserDetailsViewModel(
 
     fun logout() {
         launchRequest {
+            GoogleSignIn.getClient(get<Context>(), GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("12243085829-b4grfg7t41m9k2u8nqg4f4sgoc7m60o2.apps.googleusercontent.com")
+                .requestEmail()
+                .build()).signOut().await()
             authRepository.logout()
             _logout.emit(true)
         }
